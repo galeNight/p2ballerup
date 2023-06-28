b30debd07458383a6ae88260248209117b1ce373
galeNight <97892095+galeNight@users.noreply.github.com>
2023-06-28 14:57:25 +0200

    Add files via upload

---
+++ Quiz/Quiz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Quiz
+{
+    public class Quiz
+    {
+        public string ?Quistionstext { get; set; }
+        public List<string> Options = new List<string>();
+        public int Correctanswer { get; set; }
+        public string ?Description { get; set; }
+    }
+
+    public class ParseJson
+    {
+        public static List<Quiz> FetchFile()
+        {
+            string file = File.ReadAllText("C:\\Users\\janm\\source\\repos\\Quiz\\Qusidata.json");
+            return JsonConvert.DeserializeObject<List<Quiz>>(file);
+        }
+    }
+}
