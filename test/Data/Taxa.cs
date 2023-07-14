d7c8f2a183f9b31f2b4607b540fa870e59f0efa8
galeNight <97892095+galeNight@users.noreply.github.com>
2023-07-14 11:23:55 +0200

    Add files via upload

---
+++ test/Data/Taxa.cs
@@ -0,0 +1,13 @@
+﻿namespace test.Data
+{
+    public class Taxa
+    {
+        public string ?AlmindeligVogne { get; set; }
+        public string ?StorVogneMinibus { get; set; }
+        public string ?DagTimer { get; set; }
+        public string ?AftenNatWeekendHelidage { get; set; }
+        public int StartPris { get; set; }
+        public int KilometerPris { get; set; }
+        public int MinutPris { get; set; }
+    }
+}
