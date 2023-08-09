b941dee4e9b015d95cabe107cac0c96f42b95a1c
galeNight <97892095+galeNight@users.noreply.github.com>
2023-08-09 13:52:16 +0200

    Add files via upload

---
+++ fileprogram/fileprogram/Name.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileprogram
+{
+    //public is access modifier and class is referencetype
+    public class Name
+
+    {
+        // fild with parameter
+        private string _input;
+
+        //constrostor, access modifier referencetype and parameter
+        public Name(string input)
+        {
+            //fild with parameter
+            _input = input;
+        }
+        // int metode
+        public int Lcount()
+        {
+            //int returntype because of .length(length is a propertys that is in all strings)
+            return _input.Length;
+        }
+    }
+
+}
---
+++ fileprogram/fileprogram/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace fileprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input=Console.ReadLine();//strign input
+            Name name = new Name(input);//initialize the Name class object
+            Console.WriteLine(name.Lcount());//using the metod from name object and print the cout af characters
+        }
+    }
+}
