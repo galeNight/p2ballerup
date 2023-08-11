9ece6fb2daa973279ea80af859f9c9cdd86659a3
galeNight <97892095+galeNight@users.noreply.github.com>
2023-08-11 13:29:03 +0200

    Add files via upload

---
+++ talformat/talformat/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace talformat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //formareringer af tal 
+            int a = 25123;
+            double b = 232354.3426;
+            double c = 0.25;
+
+            Console.WriteLine(a.ToString("N2"));
+            Console.WriteLine(a.ToString("N3"));
+            Console.WriteLine(a.ToString("F1"));
+            Console.WriteLine(a.ToString("C2\n"));
+            Console.WriteLine(b.ToString("N2"));
+            Console.WriteLine(b.ToString("N3"));
+            Console.WriteLine(b.ToString("F5\n"));
+            Console.WriteLine(a.ToString("P0"));
+            Console.WriteLine(a.ToString("P2"));
+        }
+    }
+}
