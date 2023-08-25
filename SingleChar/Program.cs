98ad652dcaa7eb5ec0c58937bd70cbb8e548d879
galeNight <97892095+galeNight@users.noreply.github.com>
2023-08-25 14:17:23 +0200

    Add files via upload

---
+++ SingleChar/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SingleChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "qwer";
+            int index = 0;
+            while (index < name.Length)
+            {
+                char character = name[index];
+                Console.WriteLine(character);
+                index++;
+            }
+        }
+    }
+}
