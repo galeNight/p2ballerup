deb21ecd09e9333207bd0e3bce6bc27167954e00
galeNight <97892095+galeNight@users.noreply.github.com>
2023-08-28 14:15:38 +0200

    Add files via upload

--- SingleChar/Program.cs
+++ SingleChar/Program.cs
@@ -6,6 +6,9 @@
         {
             Program obj = new Program();
             obj.charName();
+            obj.arrayone();
+            obj.arraytwo();
+            obj.arrayname();
         }
         void charName()
         {
@@ -15,6 +18,40 @@
                 Console.WriteLine(output);
             }
             Console.WriteLine();
+        }
+        void arrayone()
+        {
+            double nedbørmåned1 = 4;
+            double nedbørmåned2 = 12;
+            double nedbørmåned3 = 2;
+            double nedbørmåned4 = 8;
+            double sum = nedbørmåned1 + nedbørmåned2 + nedbørmåned3 + nedbørmåned4;
+            Console.WriteLine($"sum{sum:N2}");
+            double gns = sum / 4;
+            Console.WriteLine($"Gennemsnit{gns:N2}");
+            Console.WriteLine();
+        }
+        void arraytwo()
+        {
+            double[] nedbør = { 4, 12, 2, 8 };
+            double sum = 0;
+            for(int i=0;i<nedbør.Length;i++)
+            {
+                sum += nedbør[i];
+                Console.WriteLine($"sum{sum:N3}");
+                double gns = sum / nedbør.Length;
+                Console.WriteLine($"Gennemsnit{gns:N2}");
+                Console.WriteLine();
+            }
+        }
+        void arrayname()
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            for(int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(name[i]);
+            }
         }
     }
 }
