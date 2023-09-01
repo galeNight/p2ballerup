4bac0a18f7b02350d76e0876eb1323dca47cbe0a
galeNight <97892095+galeNight@users.noreply.github.com>
2023-09-01 09:44:26 +0200

    Add files via upload

---
+++ htmlandcsharp/Pages/Myclass2.cs
@@ -0,0 +1,9 @@
+﻿namespace htmlandcsharp.Pages
+{
+
+    public class Myclass2
+    {
+        public string Name = "jannick";
+        public int Age = 27;
+    }
+}
---
+++ htmlandcsharp/Pages/myclass.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace htmlandcsharp.Pages
+{
+    public class myclass
+    {
+        //this is a public class that contains a string field with a name
+        public string name  = "jannick";
+    }
+}
