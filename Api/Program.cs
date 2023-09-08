29efeb401e457133abeadcbd55b0267916dad509
galeNight <97892095+galeNight@users.noreply.github.com>
2023-09-08 13:03:50 +0200

    virsual pleasing
    added day count and update as well datetime to make it virsual pleasing to the eye chance the Api class to be streamline so it became less of a mess code

--- Api/Program.cs
+++ Api/Program.cs
@@ -13,8 +13,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-api sd = new api();
-sd.tets();
 
 app.UseHttpsRedirection();
 
