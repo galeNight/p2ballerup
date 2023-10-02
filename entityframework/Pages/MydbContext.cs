334ce1b870b2639ae0289b0d719d057d0db50c36
galeNight <97892095+galeNight@users.noreply.github.com>
2023-10-02 11:46:41 +0200

    Add files via upload

---
+++ entityframework/Pages/MydbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace entityframework.Pages
+{
+    public class MydbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        public MydbContext(DbContextOptions<MydbContext>options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ entityframework/Program.cs
@@ -0,0 +1,31 @@
+using entityframework.Pages;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<MydbContext>(options =>
+{
+    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+});
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
