7d86883cdb129b66d04590b56ccd797d235d3109
galeNight <97892095+galeNight@users.noreply.github.com>
2023-10-02 13:35:59 +0200

    Add files via upload

--- entityframework/Pages/MydbContext.cs
+++ entityframework/Pages/MydbContext.cs
@@ -3,7 +3,7 @@ namespace entityframework.Pages
 {
     public class MydbContext : DbContext
     {
-        public DbSet<Person> People { get; set; }
+        public DbSet<EFPerson> People { get; set; }
 
         public MydbContext(DbContextOptions<MydbContext>options)
             : base(options)
